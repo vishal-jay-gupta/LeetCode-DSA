4ee5b7dd8366fd3a4ea4aff17ffaf86273200a1f
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-06-28 11:06:48 +0530

    Time: 0 ms (100%), Space: 46.4 MB (86.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -22,27 +22,50 @@ public class Solution {
     In worth case we are storing n node in hset
 */
 
+    // public ListNode detectCycle(ListNode head) {
+    //     if(head == null) return null;
+    //     if(head.next == null) return null;
+
+    //     HashSet<ListNode> hset = new HashSet<>();
+
+    //     while(head != null){
+    //         if(hset.contains(head)){
+    //             return head;
+    //         }else{
+    //             hset.add(head);
+    //             head = head.next;
+    //         }
+    //     }
+
+        
+    //     return null;
+    // }
+
+    
+    /* 
+    Aproach 2
+    Time Complexity = O(n)
+    Space Complexity = O(1)
+
+    Floyd's Cycle Detection - use slow and fast pointer
+*/
     public ListNode detectCycle(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        HashSet<ListNode> hset = new HashSet<>();
-
-        while(head != null){
-            if(hset.contains(head)){
-                return head;
-            }else{
-                hset.add(head);
-                head = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
         }
 
-        
         return null;
     }
-
-    
-    // public ListNode detectCycle(ListNode head) {
-        
-    // }
 }
