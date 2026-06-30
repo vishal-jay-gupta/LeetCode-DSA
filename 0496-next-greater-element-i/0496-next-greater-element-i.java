e03ffb4276804bbd693f5aadf109cb0b899df2be
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-06-30 16:11:14 +0530

    Time: 6 ms (9.27%), Space: 44.9 MB (81.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,4 +1,28 @@
 class Solution {
+    /*-------------------------------------------------------------------------------
+            Time Comlexity = O(n^2)
+    -------------------------------------------------------------------------------*/
+    // public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    //     int[] list = new int[nums1.length];
+
+    //     for(int i=0; i<nums1.length; i++){
+    //         int j = nums2.length-1;
+    //         int ans = -1;
+    //         while(j >= 0){
+    //             if(nums1[i] == nums2[j]){
+    //                 list[i] = ans;
+    //                 break;
+    //             }
+    //             if(nums2[j] > nums1[i]){
+    //                 ans = nums2[j];
+    //             }
+    //             j--;
+    //         }
+            
+    //     }
+    //     return list;
+    // }
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q2 = new LinkedList<>();
