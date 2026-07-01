0117b3a5e9360b642144cf447dd3abe0cefacca8
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-07-01 16:36:28 +0530

    Time: 15 ms (50.97%), Space: 46.9 MB (56.84%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,14 +1,51 @@
 class Solution {
+    /*-----------------------------------------------------------------------------
+                Time Complexity = O(n^2)
+                Space Complexity = O(1)
+    ------------------------------------------------------------------------------*/
+
+
+    // public int firstUniqChar(String s) {
+    //     for(int i=0; i<s.length(); i++){
+    //         for(int j=0; j<=s.length(); j++){
+    //             if(j == s.length()){
+    //                 return i;
+    //             }
+    //             if(i == j) continue;
+    //             if(s.charAt(i) == s.charAt(j)) break;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+
+    /*-----------------------------------------------------------------------------
+                Time Complexity = 
+                Space Complexity =
+    ------------------------------------------------------------------------------*/
+
     public int firstUniqChar(String s) {
+        int[] freq = new int[27];
+        // HashMap<String, Integer> hMap = new HashMap<>();
         for(int i=0; i<s.length(); i++){
-            for(int j=0; j<=s.length(); j++){
-                if(j == s.length()){
-                    return i;
-                }
-                if(i == j) continue;
-                if(s.charAt(i) == s.charAt(j)) break;
+            int later = s.charAt(i) - 'a';
+            freq[later]++;
+        }
+
+        for(int i=0; i<27; i++){
+            System.out.println(freq[i]);
+        }
+
+        for(int i=0; i<s.length(); i++){
+            int later = s.charAt(i) - 'a';
+            if(freq[later] == 1){
+                return i;
             }
         }
+
         return -1;
+
+
     }
 }
