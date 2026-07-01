8945697c66459a87102e1eed4202688234edbd3e
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-07-01 14:17:10 +0530

    Time: 6 ms (9.31%), Space: 44.5 MB (94.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -23,7 +23,36 @@ class Solution {
         return list;
     }
 
+    
+    /*-------------------------------------------------------------------------------
+            Time Comlexity = O(n)
+    -------------------------------------------------------------------------------*/
+
     // public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
+    //     int[] list = new int[nums1.length];
+    //     Stack<Integer> s = new Stack<>();
+    //     HashMap<Integer, Integer> hMap = new HashMap();
+
+    //     for(int i=0; i<nums2.length; i++){
+    //         if(s.isEmpty()){
+    //             s.push(nums2[i]);
+    //         }else{
+    //             if(s.peek() < nums2[i]){
+    //                 hMap.put(s.peek(), nums2[i]);
+    //                 s.pop();
+    //             }
+    //             s.push(nums2[i]);
+    //         }
+    //     }
+
+    //     while(!s.isEmpty()){
+    //         hMap.put(s.pop(), -1);
+    //     }
+
+    //     for(int i=0; i<nums1.length; i++){
+    //         list[i] = hMap.get(nums1[i]);
+    //     }
+
+    //     return list;
     // }
 }
