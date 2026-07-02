a95485dc7db61b3c9aedf8d38cdc579214fddc50
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-07-02 16:15:01 +0530

    Time: 2 ms (50.39%), Space: 43.1 MB (59.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,37 +1,72 @@
 import java.util.*;
 
 class Solution {
-    class Person{
-        int tickets;
-        int index;
+    /*-------------------------------------------------------------------------------
+                    APPROACH - 1
+            ------------------------------------
+            Time complexity = O(n + totalTickets)
+            Space Complexity: O(n)
+    --------------------------------------------------------------------------------*/
 
-        Person(int tickets, int index){
-            this.tickets = tickets;
-            this.index = index;
-        }
-    }
+
+    // class Person{
+    //     int tickets;
+    //     int index;
+
+    //     Person(int tickets, int index){
+    //         this.tickets = tickets;
+    //         this.index = index;
+    //     }
+    // }
+
+    // public int timeRequiredToBuy(int[] tickets, int k) {
+    //     Queue<Person> q = new LinkedList<>();
+
+    //     // 
+    //     for(int i=0; i<tickets.length; i++){
+    //         q.offer(new Person(tickets[i], i));
+    //     }
+
+    //     int ans = 0;
+    //     while(!q.isEmpty()){
+    //         Person p = q.poll();
+    //         p.tickets--;
+    //         ans++;
+    //         if(p.tickets == 0){
+    //             if(p.index == k){
+    //                 return ans;
+    //             }
+    //         }else{
+    //             q.offer(p);
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    /*-------------------------------------------------------------------------------
+                    APPROACH - 2
+            ------------------------------------
+            Time complexity = O(n + totalTickets)
+            Space Complexity: O(1)
+    --------------------------------------------------------------------------------*/
 
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Person> q = new LinkedList<>();
-
-        // 
-        for(int i=0; i<tickets.length; i++){
-            q.offer(new Person(tickets[i], i));
-        }
-
         int ans = 0;
-        while(!q.isEmpty()){
-            Person p = q.poll();
-            p.tickets--;
-            ans++;
-            if(p.tickets == 0){
-                if(p.index == k){
-                    return ans;
+        int n = tickets.length;
+         while (tickets[k] > 0) {
+            for (int i = 0; i < n; i++) {
+
+                if (tickets[i] != 0) {
+                    tickets[i]--;
+                    ans++;
                 }
-            }else{
-                q.offer(p);
+
+                if (tickets[k] == 0)
+                    return ans;
             }
         }
-        return -1;
+
+        return ans;
     }
 }
