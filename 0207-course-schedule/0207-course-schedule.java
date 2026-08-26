0ecd707df00a8650c69366f22ea47f4c1e0a80b3
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-08-27 02:55:19 +0530

    Time: 30 ms (5.08%), Space: 46.4 MB (98.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,52 +1,102 @@
 class Solution {
+    // public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    //     // Create adjacency list
+    //     ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
+    //     for (int i = 0; i < numCourses; i++) {
+    //         graph[i] = new ArrayList<>();
+    //     }
+
+    //     // Calculate indegree
+    //     int[] indegree = new int[numCourses];
+
+    //     for (int[] pair : prerequisites) {
+    //         int course = pair[0];
+    //         int prerequisite = pair[1];
+
+    //         graph[prerequisite].add(course);
+    //         indegree[course]++;
+    //     }
+
+    //     // Add courses having no preerequisits
+    //     Queue<Integer> q = new LinkedList<>();
+
+    //     for (int i = 0; i < numCourses; i++) {
+    //         if (indegree[i] == 0) {
+    //             q.offer(i);
+    //         }
+    //     }
+
+    //     // Process courses
+    //     int count = 0;
+
+    //     while (!q.isEmpty()) {
+
+    //         int curr = q.poll();
+    //         count++;
+
+    //         for (int next : graph[curr]) {
+
+    //             indegree[next]--;
+
+    //             if (indegree[next] == 0) {
+    //                 q.offer(next);
+    //             }
+    //         }
+    //     }
+
+    //     // Did we complete all courses?
+    //     return count == numCourses;
+    // }
+
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        // Create adjacency list
-        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        boolean[] vis = new boolean[numCourses];
+        boolean[] pathVisited = new boolean[numCourses];
 
-        for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        for(int i = 0; i < numCourses; i++){
 
-        // Calculate indegree
-        int[] indegree = new int[numCourses];
+            if(!vis[i]){
 
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-
-            graph[prerequisite].add(course);
-            indegree[course]++;
-        }
-
-        // Add courses having no prerequisites
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        // Process courses
-        int count = 0;
-
-        while (!q.isEmpty()) {
-
-            int curr = q.poll();
-            count++;
-
-            for (int next : graph[curr]) {
-
-                indegree[next]--;
-
-                if (indegree[next] == 0) {
-                    q.offer(next);
+                if(isCycle(i, vis, pathVisited, prerequisites)){
+                    return false;
                 }
             }
         }
 
-        // Did we complete all courses?
-        return count == numCourses;
+        return true;
+    }
+
+    public static boolean isCycle(int src, boolean[] visited, boolean[] pathVisited, int[][] prerequisites) {
+
+        visited[src] = true;
+        pathVisited[src] = true;
+
+        for(int i = 0; i < prerequisites.length; i++){
+
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+
+            // v -> u
+            if(v == src){
+
+                if(!visited[u]){
+
+                    if(isCycle(u, visited, pathVisited, prerequisites)){
+                        return true;
+                    }
+
+                }
+                else if(pathVisited[u]){
+                    return true;
+                }
+            }
+        }
+
+        pathVisited[src] = false;
+
+        return false;
     }
 }
