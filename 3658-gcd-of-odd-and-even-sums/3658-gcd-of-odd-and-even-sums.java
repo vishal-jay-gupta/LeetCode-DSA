e121b0b7d8e3c263583e530cc91c14a14731b42a
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-09-09 15:48:26 +0530

    Time: 0 ms (100%), Space: 42.6 MB (59.64%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,15 +1,25 @@
 class Solution {
-    public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
-        int odd = 1;
-        int even = 2;
+    // public int gcdOfOddEvenSums(int n) {
+    //     int sumOdd = 0;
+    //     int sumEven = 0;
+    //     int odd = 1;
+    //     int even = 2;
 
-        while(0 < n){
-            sumEven += even;
-            sumOdd += odd;
-            n--;
-        }
-        return sumEven - sumOdd;
+    //     while(0 < n){
+    //         even += 2;
+    //         odd += 2;
+    //         sumEven += even;
+    //         sumOdd += odd;
+    //         n--;
+    //     }
+
+    //     System.out.println(sumEven);
+    //     System.out.println(sumOdd);
+    //     return sumEven - sumOdd;
+    // }
+
+
+    public int gcdOfOddEvenSums(int n) {
+        return n;
     }
 }
