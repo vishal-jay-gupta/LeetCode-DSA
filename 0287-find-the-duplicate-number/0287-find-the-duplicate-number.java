7a53db7c2e7097f16462e9165c19e5f795423bd2
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-09-10 12:54:11 +0530

    Time: 21 ms (34.1%), Space: 94.8 MB (6.53%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    /*----------------------------------------------------------------------------------------------------
+            Time Complexity = O(n log n);
+            Space Complexity = O(1);
+    -----------------------------------------------------------------------------------------------------*/
+    // public int findDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+
+    //     for(int i=1; i<nums.length; i++){
+    //         if(nums[i] == nums[i-1]){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    /*----------------------------------------------------------------------------------------------------
+            Time Complexity = O(n);
+            Space Complexity = O(n);
+    -----------------------------------------------------------------------------------------------------*/
+    public int findDuplicate(int[] nums) {
+        Map<Integer, Integer> hMap = new HashMap<>();
+        int n = nums.length;
+
+        for(int i=0; i<n; i++){
+            if(!hMap.containsKey(nums[i])){
+                hMap.put(nums[i], i);
+            }else{
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
