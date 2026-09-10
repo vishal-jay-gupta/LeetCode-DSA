816094bb8e359cecb4b131ca00731253c31f8123
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-09-10 13:39:00 +0530

    Time: 4 ms (92.39%), Space: 82.7 MB (78.47%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -21,18 +21,43 @@ class Solution {
             Time Complexity = O(n);
             Space Complexity = O(n);
     -----------------------------------------------------------------------------------------------------*/
-    public int findDuplicate(int[] nums) {
-        Map<Integer, Integer> hMap = new HashMap<>();
-        int n = nums.length;
+    // public int findDuplicate(int[] nums) {
+    //     Map<Integer, Integer> hMap = new HashMap<>();
+    //     int n = nums.length;
 
-        for(int i=0; i<n; i++){
-            if(!hMap.containsKey(nums[i])){
-                hMap.put(nums[i], i);
-            }else{
-                return nums[i];
-            }
+    //     for(int i=0; i<n; i++){
+    //         if(!hMap.containsKey(nums[i])){
+    //             hMap.put(nums[i], i);
+    //         }else{
+    //             return nums[i];
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+
+    /*----------------------------------------------------------------------------------------------------
+        Optimal Approach: ->
+            Floyd's Cycle Detection
+                Time Complexity = O(n);
+                Space Complexity = O(1);
+    -----------------------------------------------------------------------------------------------------*/
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
 }
