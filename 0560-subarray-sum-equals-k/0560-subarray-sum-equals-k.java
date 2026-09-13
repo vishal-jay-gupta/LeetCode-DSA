eb68d545672df2a7d52b65d73438847a0bfa3bd7
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-09-13 14:54:42 +0530

    Time: 24 ms (75.37%), Space: 49.2 MB (12.21%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -29,18 +29,46 @@ class Solution {
     // }
 
 
-    public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
+    /*-------------------------------------------------------------------------------------------
+            Time Complexity = O(n ^ 2)
+            Space Complexity = O(1)
+    ------------------------------------------------------------------------------------------*/
+    // public int subarraySum(int[] nums, int k) {
+    //     int n = nums.length;
 
+    //     int count = 0;
+    //     for(int i=0; i<n; i++){
+    //         int sum = 0;
+    //         for(int j=i; j<n; j++){
+    //             sum += nums[j];
+    //             if(sum == k){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+
+    //     return count;
+    // }
+
+    /*-------------------------------------------------------------------------------------------
+            Time Complexity = O(n)
+            Space Complexity = O(n)
+    ------------------------------------------------------------------------------------------*/
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> hMap = new HashMap<>();
+
+        hMap.put(0, 1);
+        int prefixSum = 0;
         int count = 0;
-        for(int i=0; i<n; i++){
-            int sum = 0;
-            for(int j=i; j<n; j++){
-                sum += nums[j];
-                if(sum == k){
-                    count++;
-                }
+        for(int i=0; i<nums.length; i++){
+            prefixSum += nums[i];
+            int remove = prefixSum - k;
+
+            if(hMap.containsKey(remove)){
+                count += hMap.get(remove);
             }
+
+            hMap.put(prefixSum, hMap.getOrDefault(prefixSum, 0) + 1);
         }
 
         return count;
