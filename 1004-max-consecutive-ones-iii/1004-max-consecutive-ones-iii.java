a6a3c7ca078d7163489c03120d99d41ab4d5e293
Vishal Gupta <166972622+vishal-jay-gupta@users.noreply.github.com>
2026-09-15 22:04:32 +0530

    Time: 3 ms (95.87%), Space: 52.1 MB (73.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,26 +1,92 @@
 class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int n = nums.length;
-        int ans = 0;
+    /*---------------------------------------------------------------------------------------------------------
+            Time Complexity = O(n^2)
+            Space Complexity = O(1)
+    --------------------------------------------------------------------------------------------------------*/
 
-        for(int i=0; i<n; i++){
-            int temp = k;
-            int curr = 0;
+    // public int longestOnes(int[] nums, int k) {
+    //     int n = nums.length;
+    //     int ans = 0;
 
-            for(int j=i; j<n; j++){
+    //     for(int i=0; i<n; i++){
+    //         int temp = k;
+    //         int curr = 0;
+
+    //         for(int j=i; j<n; j++){
                 
-                if(nums[j] == 0){
-                    if(temp == 0) break;
-                    temp--;
-                }
-                curr++;
+    //             if(nums[j] == 0){
+    //                 if(temp == 0) break;
+    //                 temp--;
+    //             }
+    //             curr++;
+    //         }
+
+    //         ans = Math.max(curr, ans);
+
+
+    //     }
+
+    //     return ans;
+    // }
+
+    /*---------------------------------------------------------------------------------------------------------
+            Time Complexity = O(2n) = O(n)
+            Space Complexity = O(1)
+    --------------------------------------------------------------------------------------------------------*/
+
+
+    public int longestOnes(int[] nums, int k) {
+        
+        int i=0; 
+        int j=0;
+        int zeros = 0;
+        int maxLength = 0;
+
+        while(j<nums.length){
+            if(nums[j] == 0) zeros++;
+
+            while(zeros > k){
+                if(nums[i] == 0) zeros--;
+                i++;
             }
 
-            ans = Math.max(curr, ans);
-
-
+            if(zeros <= k){
+                maxLength = Math.max(maxLength, j-i+1);
+            }
+            j++;
         }
 
-        return ans;
+        return maxLength;
     }
+
+
+    /*---------------------------------------------------------------------------------------------------------
+            Time Complexity = O(n)
+            Space Complexity = O(1)
+    --------------------------------------------------------------------------------------------------------*/
+
+
+    // public int longestOnes(int[] nums, int k) {
+        
+    //     int i=0; 
+    //     int j=0;
+    //     int zeros = 0;
+    //     int maxLength = 0;
+
+    //     while(j<nums.length){
+    //         if(nums[j] == 0) zeros++;
+
+    //         while(zeros > k){
+    //             if(nums[i] == 0) zeros--;
+    //             i++;
+    //         }
+
+    //         if(zeros <= k){
+    //             maxLength = Math.max(maxLength, j-i+1);
+    //         }
+    //         j++;
+    //     }
+
+    //     return maxLength;
+    // }
 }
